78fe063e86f0ff22aa432901f8af8905ebe62e75
Taylor Mravic <taylor.mravic@gmail.com>
2022-03-01 23:02:30 +0900

    Learned how to implement a search feature

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,23 +1,54 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [:show, :create, :edit, :destroy]
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
-    @flats = Flat.all
-    @flat = Flat.new
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?","%#{params[:query]}%")
+    else
+      @flats = Flat.all
+    end
   end
 
   def new
     @flat = Flat.new
+  end
 
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      redirect_to flats_path, notice: "Please try again"
+    end
   end
 
   def show
+  end
 
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :photo)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, presence: true
   has_one_attached :photo, dependent: :destroy
 end
